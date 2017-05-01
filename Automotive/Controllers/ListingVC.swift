43f55f3aca1250b6ed3e9358984c9efa3ecc1e02
Mac <mac@macs-macbook-pro.local>
2017-05-01 15:36:39 +0400

    Lunch Break

--- Automotive/Controllers/ListingVC.swift
+++ Automotive/Controllers/ListingVC.swift
@@ -72,7 +72,7 @@ class ListingVC: UIViewController {
     
     
     /*
-    // MARK: - Navigation
+    // MARK: - Navigations
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
