5e7e6a466d626a5267c0883662f68fdd276196fc
Mac <mac@macs-macbook-pro.local>
2017-05-01 22:20:52 +0400

    1. Added an AlertController to Delete Button 2. Added Edit Mode for UITableViewCell 3. Fixed the Empty Label

--- Automotive/Controllers/FavouriteVC.swift
+++ Automotive/Controllers/FavouriteVC.swift
@@ -17,10 +17,13 @@ class FavouriteVC: UIViewController {
     
     var vehicles = [FavViewModel]()
     
+    var messageLabel:UILabel?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         favTableView.register(UINib(nibName: "FavTableViewCell", bundle: nil), forCellReuseIdentifier: FavouriteVC.cellIdentifier)
         favTableView.tableFooterView = UIView(frame: CGRect.zero)
+        self.emptyTable()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -36,10 +39,12 @@ class FavouriteVC: UIViewController {
             }
             
             if searchResult.count > 0 {
-                self.favTableView.reloadData()
+                messageLabel?.isHidden = true
             } else {
-                emptyTable()
+                messageLabel?.isHidden = false
             }
+            
+            self.favTableView.reloadData()
             
         } catch {
             print("Error")
@@ -85,14 +90,51 @@ extension FavouriteVC: UITableViewDelegate, UITableViewDataSource {
         return cell
     }
     
+    
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        let delete = UITableViewRowAction(style: .destructive, title: "Delete") { (action, indexPath) in
+            self.deleteVehicle(index: indexPath.row)
+            // delete item at indexPath
+        }
+        
+        return [delete]
+    }
+    
+    // MARK: Perform Segue for Map Address
+    
     func deleteFavourites(sender:AnyObject) {
-        let deleteButton = sender as! UIButton
-        let viewModel: FavViewModel = vehicles[deleteButton.tag]
+        
+        let actionSheetController: UIAlertController = UIAlertController(title: "Delete Favourite", message: "Are you sure you want to delete this from Favourites?", preferredStyle: .alert)
+        
+        let cancelAction: UIAlertAction = UIAlertAction(title: "Cancel", style: .cancel) { action -> Void in
+            //Just dismiss the action sheet
+        }
+        
+        let okAction: UIAlertAction = UIAlertAction(title: "Ok", style: .default) { action -> Void in
+            let deleteButton = sender as! UIButton
+            self.deleteVehicle(index: deleteButton.tag)
+        }
+        
+        actionSheetController.addAction(cancelAction)
+        actionSheetController.addAction(okAction)
+        self.present(actionSheetController, animated: true, completion: nil)
+    }
+    
+    func deleteVehicle(index: Int) {
+        let viewModel: FavViewModel = self.vehicles[index]
         viewModel.deleteFavourite()
-        vehicles.remove(at: deleteButton.tag)
+        self.vehicles.remove(at: index)
+        
+        if self.vehicles.count == 0 {
+            self.messageLabel?.isHidden = false
+        } else {
+            self.messageLabel?.isHidden = true
+        }
+        
         self.favTableView.reloadData()
     }
     
+    // MARK: Perform Segue for Map Address
     
     func displayMapAddress(sender:AnyObject) {
         let mapButton = sender as! UIButton
@@ -108,22 +150,21 @@ extension FavouriteVC: UITableViewDelegate, UITableViewDataSource {
             let nav = segue.destination as! UINavigationController
             let details = nav.topViewController as! MapAddressVC
             details.address = viewModel.addressText
-            
         }
     }
     
     // MARK: Empty Table
     
     func emptyTable() {
-        let messageLabel:UILabel = UILabel(frame: CGRect(x: 0, y: 0, width: self.view.bounds.size.width, height: self.view.bounds.size.height))
-        messageLabel.textColor = UIColor.lightGray
-        messageLabel.numberOfLines = 0
-        messageLabel.textAlignment = .center
-        messageLabel.font = UIFont.systemFont(ofSize: 18)
-        messageLabel.sizeToFit()
-        messageLabel.text = "You don't have any favorites yet. All your favorites will show up here."
+        messageLabel = UILabel(frame: CGRect(x: 0, y: 0, width: self.view.bounds.size.width, height: self.view.bounds.size.height))
+        messageLabel?.textColor = UIColor.lightGray
+        messageLabel?.numberOfLines = 0
+        messageLabel?.textAlignment = .center
+        messageLabel?.font = UIFont.systemFont(ofSize: 18)
+        messageLabel?.sizeToFit()
+        messageLabel?.isHidden = false
+        messageLabel?.text = "You don't have any favorites yet. All your favorites will show up here."
         self.favTableView.backgroundView = messageLabel
     }
-    
 }
 
